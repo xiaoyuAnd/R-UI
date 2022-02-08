b75a28f23f5f3bc1ea1f68699deec7385e41c5ee
Frank <2521647197@qq.com>
2022-02-08 20:39:30 +0800

    更新package.json

---
+++ index.js
@@ -0,0 +1,20 @@
+import Button from "./button.vue";
+import Icon from "./icon.vue";
+import ButtonGroup from "./button-group"
+import Input from "./input"
+import Row from "./row";
+import Col from "./col";
+import Layout from "./layout"
+import Header from "./header";
+import Content from "./content";
+import Sider from "./sider";
+import Footer from "./footer";
+import Tabs from "./tabs";
+import TabsHead from "./tabs-header";
+import TabsItem from "./tabs-item";
+import TabsPane from "./tabs-pane";
+import TabsBody from "./tabs-body";
+import Toast from "./toast";
+import plugin from "./plugin";
+
+export {Button,Icon,ButtonGroup,Input,Row,Col,Layout,Header,Content,Sider,Footer,Tabs,TabsHead,TabsItem,TabsPane,TabsBody,Toast,plugin}
